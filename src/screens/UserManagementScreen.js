a7052ab2f891540b8bbcb9489c01ebda1e1b0a8a
Anpsujwal <anpsujwal@gmail.com>
2025-08-03 20:01:01 +0530

    added ScrollView

--- src/screens/UserManagementScreen.js
+++ src/screens/UserManagementScreen.js
@@ -144,7 +144,7 @@ export default function UserManagementScreen() {
   return (
     <LinearGradient colors={['#ece9e6', '#ffffff']} style={styles.gradient}>
       <GoBackToDashboard />
-      <View contentContainerStyle={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.heading}>User Management</Text>
 
         <View style={styles.form}>
@@ -284,7 +284,7 @@ export default function UserManagementScreen() {
         ) : (
           <Text style={styles.noUsersText}>No users created</Text>
         )}
-      </View>
+      </ScrollView>
     </LinearGradient>
   );
 }
